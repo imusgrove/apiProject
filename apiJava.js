639d9d0e95e47c85fe485b64ea1e14a59c01d4cc
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-05-29 13:38:05 -0400

    js change

--- apiJava.js
+++ apiJava.js
@@ -1,83 +1,38 @@
-//unsplash syntax
-// ES Modules syntax
-// import Unsplash from 'unsplash-js';
+  var randomImage = document.querySelector('.random-image');
+  console.log("randomImage:", randomImage);
 
-// require syntax
-// const unsplash = require('unsplash-js').default;
+  fetch('https://source.unsplash.com/collections/582659:')
+  .then(function(response){
+    if(!response.ok){
+      console.log(respone);
+      return new Error(response);
+    }
+    console.log("Response:", response);
+    return response.blob();
+  })
+  .then(function(photoBlob){
+    console.log("My Blob:", photoBlob)
+    var objectURL = URL.createObjectURL(photoBlob);
+    console.log("Object URL:", objectURL);
+    randomImage.src = objectURL;
+  })
 
-//urls
-const baseURL = 'https://unsplash.com/collections/582659';
-const key = '40995db20b2a0fec48a757d13d9011af752931ea751de85e23f07549da9aabec';
-let url;
+  //while loop to get different pictures
+  do{
 
-//DOM elements-search form
-const searchPhoto = document.querySelector('.search');
-const submitBtn = document.querySelector('.submit');
-const searchForm = document.querySelector('.form');
+  }
+  while (i < 1000);
 
-//DOM Elemetns-navigation
-const nextBtn = document.querySelector('.next')
-const previousBtn = document.querySelector('.prev');
-const nav = document.querySelector('nav');
+  .catch(function(err){
+    console.log(err);
+  })
 
-//results section
-const section = document.querySelector('section');
-
-//initial nav style
-// nav.style.display = 'none';
-
-//page display
-let pageNumber = 0;
-let displayNav = false;
-
-// const unsplashPics = new Unsplash({
-//   applicationId: "40995db20b2a0fec48a757d13d9011af752931ea751de85e23f07549da9aabec",
-//   secret: "383659c5a0180c8de49aed967977a88af4e1ee7f4ed2f5f92f55dfae08f55b6e",
-//   callbackUrl: "urn:ietf:wg:oauth:2.0:oob"
-// });
-
-// event listeners
-// searchPhoto.addEventListener('submit', fetchResults);
-// nextBtn.addEventListener('click', nextPage);
-// previousBtn.addEventListener('click', previousPage);
-
-//fetching results
-function fetchResults(e){
-console.log(e);
-url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchPhoto.value;
-}
-
-function nextPage(){
-  console.log("next button clicked");
-}
-
-function previousPage(){
-  console.log("next button clicked");
-}
-
-
-
-  //tilted page scroll
-  // $(".main").tiltedpage_scroll({
-  //   sectionContainer: "> section",     // In case you don't want to use <section> tag, you can define your won CSS selector here
-  //   angle: 50,                         // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
-  //   opacity: true,                     // You can toggle the opacity effect with this option. The default value is true
-  //   scale: true,                       // You can toggle the scaling effect here as well. The default value is true.
-  //   outAnimation: true                 // In case you do not want the out animation, you can toggle this to false. The defaul value is true.
-  // });
-
-  //error handling
-  // unsplash.users.profile("naoufal")
-  // .catch(err => {
-  //   // Your flawless error handling code
-  // });
-
-  //search photos by keyword
-  // unsplash.search.photos("dogs", 1)
-  // .then(toJson)
-  // .then(json => {
-  //   // Your code
-  // });
-///tutorial
-
+  //tilted scroll
+  $(".main").tiltedpage_scroll({
+    sectionContainer: "> section",     // In case you don't want to use <section> tag, you can define your won CSS selector here
+    angle: 50,                         // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
+    opacity: true,                     // You can toggle the opacity effect with this option. The default value is true
+    scale: true,                       // You can toggle the scaling effect here as well. The default value is true.
+    outAnimation: true                 // In case you do not want the out animation, you can toggle this to false. The defaul value is true.
+  });
 
