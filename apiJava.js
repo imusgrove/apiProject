20f3a175d1ba251043d3b855611693f98301b81a
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-05-29 17:03:43 -0400

    fixed it

--- apiJava.js
+++ apiJava.js
@@ -1,7 +1,10 @@
+  
   var randomImage = document.querySelector('.random-image');
   console.log("randomImage:", randomImage);
-
-  fetch('https://source.unsplash.com/collections/582659:')
+//functions to get pictures
+  
+function newPhoto() {
+fetch(`https://source.unsplash.com/random?sig=${Math.random()*1000}`)
   .then(function(response){
     if(!response.ok){
       console.log(respone);
@@ -16,23 +19,12 @@
     console.log("Object URL:", objectURL);
     randomImage.src = objectURL;
   })
-
-  //while loop to get different pictures
-  do{
-
-  }
-  while (i < 1000);
-
   .catch(function(err){
     console.log(err);
   })
 
-  //tilted scroll
-  $(".main").tiltedpage_scroll({
-    sectionContainer: "> section",     // In case you don't want to use <section> tag, you can define your won CSS selector here
-    angle: 50,                         // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
-    opacity: true,                     // You can toggle the opacity effect with this option. The default value is true
-    scale: true,                       // You can toggle the scaling effect here as well. The default value is true.
-    outAnimation: true                 // In case you do not want the out animation, you can toggle this to false. The defaul value is true.
-  });
+randomImage.addEventListener('click', newPhoto)
+function newPhoto(e){
 
+}
+}
