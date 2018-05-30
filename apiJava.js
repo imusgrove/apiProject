ca2f18938a512f6165c1f189cae5e0c17dacece3
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-05-30 09:13:04 -0400

    random changes

--- apiJava.js
+++ apiJava.js
@@ -1,30 +1,61 @@
-  
-  var randomImage = document.querySelector('.random-image');
-  console.log("randomImage:", randomImage);
-//functions to get pictures
-  
+
+var randomImage = document.querySelector('.random-image');
+console.log("randomImage:", randomImage);
+
+
+//functions to get pictures 
 function newPhoto() {
-fetch(`https://source.unsplash.com/random?sig=${Math.random()*1000}`)
-  .then(function(response){
-    if(!response.ok){
-      console.log(respone);
-      return new Error(response);
-    }
-    console.log("Response:", response);
-    return response.blob();
-  })
-  .then(function(photoBlob){
-    console.log("My Blob:", photoBlob)
-    var objectURL = URL.createObjectURL(photoBlob);
-    console.log("Object URL:", objectURL);
-    randomImage.src = objectURL;
-  })
-  .catch(function(err){
-    console.log(err);
-  })
+  fetch(`https://source.unsplash.com/random?sig=${Math.random() * 1000}`)
+    .then(function (response) {
+      if (!response.ok) {
+        console.log(respone);
+        return new Error(response);
+      }
+      console.log("Response:", response);
+      return response.blob();
+    })
+    .then(function (photoBlob) {
+      console.log("My Blob:", photoBlob)
+      var objectURL = URL.createObjectURL(photoBlob);
+      console.log("Object URL:", objectURL);
+      randomImage.src = objectURL;
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
 
-randomImage.addEventListener('click', newPhoto)
-function newPhoto(e){
+  // button function
+  // randomImage.addEventListener('click', newPhoto)
+  // function newPhoto(e){
 
+  // }
 }
-}
+
+// crossfade
+var img, i = 1;
+var nImg = 1000;
+div = document.getElementById('foo');
+img = new Image();
+div.appendChild(img);
+img.src = 'https://source.unsplash.com/random?sig=${Math.random()*1000}' + i + '.jpg';
+img.id = "frames";
+
+var interval = setInterval(function () {
+  i++;
+  if (i > nImg) {
+    i = 1;
+  }
+
+  var element = document.getElementById("frames");
+  element.outerHTML = "";
+  delete element;
+
+  if (i > nImg) {
+    clearInterval(interval);
+  } else {
+    img = new Image();
+    div.appendChild(img);
+    img.src = 'https://source.unsplash.com/random?sig=${Math.random()*1000}' + i + '.jpg';
+    img.id = "frames";
+  }
+}, 3000);
